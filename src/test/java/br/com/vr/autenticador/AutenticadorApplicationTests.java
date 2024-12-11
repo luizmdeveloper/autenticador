edd590a8ccda76c437cb92aa3b498defc172d938
Luiz Mário <azure@inforio.com.br>
2024-12-10 23:11:13 -0300

    commit initial

---
+++ src/test/java/br/com/vr/autenticador/AutenticadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vr.autenticador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
